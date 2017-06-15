0fe5fa861633ca016687ed8b58ac0cc7c949c5a3
operatorequals <john.torakis@gmail.com>
2017-06-15 13:04:22 +0400

    trying to fix ShellHandler test

--- tests/test_SimpleShellHandler.py
+++ tests/test_SimpleShellHandler.py
@@ -12,46 +12,46 @@ in_length = 4
 
 
 orch1 = SimpleOrchestrator( "passphrase",
-    2, out_length, in_length,
-    # cycling_algorithm = sha512
-    )
+	2, out_length, in_length,
+	# cycling_algorithm = sha512
+	)
 
 orch2 = SimpleOrchestrator( "passphrase",
-    2, out_length, in_length,
-    # cycling_algorithm = sha512,
-    reverse = True)
+	2, out_length, in_length,
+	# cycling_algorithm = sha512,
+	reverse = True)
 
 chunks = []
 def dummy_receive( ) :
-    while not chunks :
-        sleep(0.1)
-    # print "Receiving"
-    return chunks.pop(0)
+	while not chunks :
+		sleep(0.1)
+	# print "Receiving"
+	return chunks.pop(0)
 
 
 testable = None
 
 def dummy_send( raw ) :
-    global testable
-    # print "sending!"
-    stream, message = orch1.depositChunk( raw )
-    if message :
-        testable = message
+	global testable
+	# print "sending!"
+	stream, message = orch1.depositChunk( raw )
+	if message :
+		testable = message
 
 
 
 class Test_ShellHandler (unittest.TestCase) :
 
-    def setUp( self ) :
-        self.p_handler = SimpleShellHandler( dummy_receive, dummy_send, orch2 )
+	def setUp( self ) :
+		self.p_handler = SimpleShellHandler( dummy_receive, dummy_send, orch2 )
 
-    def test_shell_usage( self, ) :
-        echoed = '111111111111'
-        chunk = orch1.readyMessage( "echo '%s' " % echoed, 'main' )
+	def test_shell_usage( self, ) :
+		echoed = '111111111111'
+		chunk = orch1.readyMessage( "echo '%s' " % echoed, 'main' )
 
-        chunks.extend( chunk )
-        # sleep(0.9)
-        # print '======================================================='
-        # print testable
+		chunks.extend( chunk )
+		# sleep(0.9)
+		# print '======================================================='
+		# print testable
 		while not testable : sleep(0.5)
-        self.failUnless( testable.strip() == echoed )
+		self.failUnless( testable.strip() == echoed )
