18417a5256c0c18e24b5cd1d7fde52c7ceee2ea0
operatorequals <john.torakis@gmail.com>
2017-08-05 11:43:28 +0300

    Changed doc theme to RTD

--- docs/conf.py
+++ docs/conf.py
@@ -94,9 +94,9 @@ todo_include_todos = True
 #
 # html_theme = 'classic'
 # html_theme = 'alabaster'
-html_theme = 'pyramid'
+# html_theme = 'pyramid'
 # html_theme = 'haiku'
-
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
