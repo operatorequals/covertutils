4beb4ea5ee6c6d208d3f6a561bf1a124cf4d6a61
operatorequals <john.torakis@gmail.com>
2017-08-22 23:19:48 +0400

    Made all cycling algorithms Py2/3 compatible

--- covertutils/crypto/algorithms/standardcyclingalgorithm.py
+++ covertutils/crypto/algorithms/standardcyclingalgorithm.py
@@ -8,51 +8,69 @@ from copy import deepcopy
 
 class StandardCyclingAlgorithm ( CyclingAlgorithm ) :
 
-	__b1 = '\x55' # 0 1 0 1  0 1 0 1
-	__b2 = '\xAA' # 1 0 1 0  1 0 1 0
-	__b3 = '\xf0' # 1 1 1 1  0 0 0 0
-	__b4 = '\x0f' # 0 0 0 0  1 1 1 1
-	__b5 = '\x3c' # 0 0 1 1  1 1 0 0
-	__b6 = '\xc3' # 1 1 0 0  0 0 1 1
-	__b7 = '\x1e' # 0 0 0 1  1 1 1 0
-	__b8 = '\xc3' # 0 1 1 1  1 0 0 0
-	__b_list = [
-	__b1,
-	__b2,
-	__b3,
-	__b4,
-	__b5,
-	__b6,
-	__b7,
-	__b8 ]
+	__b1 = b'\x55' # 0 1 0 1  0 1 0 1
+	__b2 = b'\xAA' # 1 0 1 0  1 0 1 0
+	__b3 = b'\xf0' # 1 1 1 1  0 0 0 0
+	__b4 = b'\x0f' # 0 0 0 0  1 1 1 1
+	__b5 = b'\x3c' # 0 0 1 1  1 1 0 0
+	__b6 = b'\xc3' # 1 1 0 0  0 0 1 1
+	__b7 = b'\x1e' # 0 0 0 1  1 1 1 0
+	__b8 = b'\xc3' # 0 1 1 1  1 0 0 0
+	# __b_list = bytes([
+	# b'\x55', # 0 1 0 1  0 1 0 1
+	# b'\xAA', # 1 0 1 0  1 0 1 0
+	# b'\xf0', # 1 1 1 1  0 0 0 0
+	# b'\x0f', # 0 0 0 0  1 1 1 1
+	# b'\x3c', # 0 0 1 1  1 1 0 0
+	# b'\xc3', # 1 1 0 0  0 0 1 1
+	# b'\x1e', # 0 0 0 1  1 1 1 0
+	# b'\xc3', # 0 1 1 1  1 0 0 0
+	# ])
+	__b_list = bytearray(
+		__b1 +
+		__b2 +
+		__b3 +
+		__b4 +
+		__b5 +
+		__b6 +
+		__b7 +
+		__b8 +
+	b'')
+
 
 	def __init__( self, message, length = 32, cycles = 20 ) :
 
-		super( StandardCyclingAlgorithm, self ).__init__( message )
+		# print (len(self.__b_list))
+		try :
+			super( StandardCyclingAlgorithm, self ).__init__( bytearray(message, 'utf8') )
+		except TypeError :
+			super( StandardCyclingAlgorithm, self ).__init__( message )
 		self.length = length
 		self.cycles = cycles
 
 
-	# def __cycler( self, s, result, reverse = False ) :
-	# 	for c1_i, c1 in enumerate( s ) :
-	# 		mod = ( c1 + len( result ) + c1_i) % len( self.__b_list )
-	# 		if reverse :
-	# 			h1 = ord( sxor( chr(c1), self.__b_list[mod] ) )
-	# 		else :
-	# 			h1 = ord(sxor( chr(c1), self.__b_list[ (len(self.__b_list) - 1) - mod ] ) )
-	#
-	# 		if  ( h1 % 2 ) ^ reverse:
-	# 			result.insert(len( result ), h1)
-	# 		else :
-	# 			result.insert(0, h1)
-	# 	return result
+	def __cycler( self, sub_array, result, reverse = False ) :
+
+		for c1_i in range(len(sub_array)) :
+			c1 = sub_array[c1_i]
+			mod = ( int(c1) + len( result) + c1_i ) % len(self.__b_list)
+			if reverse :
+				mod = (len(self.__b_list) - 1) - mod
+			# print (c1, self.__b_list[mod])
+			h1 = c1 ^ self.__b_list[mod]
+			# print (h1)
+			if bool(int(h1) % 2) == reverse :
+				result.insert(0, h1)
+			else :
+				result.append(h1)
+		return result
 
 
 	def digest( self ) :
 
 		message = self.message
 		prev_result = message
-		__result = ''
+		__result = bytearray()
 
 		length = self.length
 		cycles = self.cycles
@@ -66,52 +84,44 @@ class StandardCyclingAlgorithm ( CyclingAlgorithm ) :
 				if cycle % 2 :
 					s1, s2 = s2, s1
 
-				for c1_i in range(len(s1)) :
-					c1 = s1[c1_i]
-					mod = (ord(c1) + len( __result) + c1_i) % 6
-					h1 = sxor( c1, self.__b_list[mod] )
-					if ord(h1) % 2 :
-						__result = h1 + __result
-					else :
-						__result = __result + h1
+				# print (s1, s2)
+				self.__cycler(s1, __result)
+				self.__cycler(s2, __result, True)
+				# print ("cycled")
+				# print (s1, s2)
 
-				for c2_i in range(len(s2)) :
-					c2 = s2[c2_i]
-					mod = (ord(c2) + len( __result ) + c2_i) % 6
-					h2 = sxor( c2, self.__b_list[ (len(self.__b_list) - 1) - mod ] )
-					if ord(h2) % 2 :
-						__result = __result + h2
-					else :
-						__result = h2 + __result
-
-
+				# print (__result)
 				d = length - len( __result )
+				if d == 0 :
+					break
 
-				__res_temp = ''
+				__res_temp = bytearray()
 				permut_list = []
-				if d != 0 :
-					for i in range( abs( d ) ) :
-						c = __result[i % len(__result)]
-						x = ord(c) % len( prev_result )
-						# print x
-						permut_list.append(x)
-						# print permut_list
-					if d > 0 :		# need more to reach Length
-						new_byte_list = permutate( prev_result, permut_list)
-						new_bytes = ''.join(new_byte_list)
-						__result = __result + new_bytes
-						# print new_bytes
-					else :
-						__result_list = bytearray(__result)
-						for x in permut_list :
 
-							__result_list.pop( x % len(__result_list) )
-							# pass
-						__result = str(__result_list)
+				for i in range( abs( d ) ) :
+					c = __result[i % len(__result)]
+					x = c % len( prev_result )
+					# print x
+					permut_list.append(x)
+					# print permut_list
+				if d > 0 :		# need more to reach Length
+					new_bytes = bytearray(permutate( prev_result, permut_list ))
+					__result = __result + new_bytes
+					# print new_bytes
+				else :
+					__result_list = bytearray(__result)
+					for x in permut_list :
+
+						__result_list.pop( x % len(__result_list) )
+						# pass
+					__result = bytearray(__result_list)
 
 
 			prev_result = __result[:]
 			portion = int((len(__result) * (float(cycle)/cycles)))
+			# print (cycle, cycles)
+			# print ( float(cycle)/cycles)
+			# print ( portion)
 			__result = __result[: portion]
 
 		return __result
