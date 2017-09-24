54ec4855b6f9a9d345f7c1f40a994b1b5e82dfa5
operatorequals <john.torakis@gmail.com>
2017-09-24 16:44:08 +0400

    Added 'stdin' in crypto package executables

--- covertutils/crypto/keys/__main__.py
+++ covertutils/crypto/keys/__main__.py
@@ -2,7 +2,7 @@
 from covertutils.crypto.keys import *
 from covertutils.crypto.algorithms import *
 
-import binascii, base64
+import binascii, base64, sys
 
 algo_dict = {
 		'null' : NullCyclingAlgorithm,
@@ -17,7 +17,7 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("key_type", help = 'The algorithm name to use', choices = algo_dict.keys(), type = str, default = 'std' )
 parser.add_argument("passphrase", help = "The passphrase for key generation", type = str)
-parser.add_argument("message", help = "The message to be encrypted", type = str)
+parser.add_argument("message", help = "The message to be encrypted", type = str, default = '-')
 
 parser.add_argument('--decrypt', '-d', help = 'Add if the message is in encrypted form', action = 'store_true', default = False, )
 parser.add_argument('--input-type', '-i', help = 'Specify the form of the input', choices = ['b64', 'hex', 'plain'], default = 'plain')
@@ -32,6 +32,9 @@ key = StandardCyclingKey(args.passphrase, cycling_algorithm = algo)
 func = key.encrypt
 if args.decrypt :
 	func = key.decrypt
+
+if args.message == '-' :
+	args.message = sys.stdin.read()
 
 if args.input_type == 'hex' :
 	message = str(binascii.unhexlify(args.message))
